6a258c019b9d743af437fbb65994ac698f1a8750
AbhinavSachan <abhinavsachan02022006@gmail.com>
2026-06-18 13:36:55 +0530

    Add Remove Duplicates solution

---
+++ 02_Lists_Basics/02_Remove_Duplicates.py
@@ -0,0 +1,12 @@
+x = int(input("Enter the size of the list : "))
+t = []
+n = []
+for i in range(0,x):
+    m = int(input("Enter the no. : "))
+    n.append(m)
+
+for num in n:
+    if num not in t:
+        t.append(num)
+    
+print(t)
