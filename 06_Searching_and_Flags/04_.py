3482187b0c51f696a7997bd034a18816d292fab7
AbhinavSachan <abhinavsachan02022006@gmail.com>
2026-06-24 23:28:16 +0530

    Add Character Frequency Counter

--- 06_Searching_and_Flags/04_.py
+++ 06_Searching_and_Flags/04_.py
@@ -1,11 +1,25 @@
 n = input("Enter the word : ")
 
 visited = []
+found = False
+
 for i in range(len(n)):
+
     if n[i] not in visited:
+
         count = 0
+
         for j in range(len(n)):
             if n[i] == n[j]:
                 count += 1
+
         visited.append(n[i])
-        print(n[i], "->", count)
+
+        if count == 1:
+            found = True
+            print(n[i], "is the first non repeated character")
+            break
+
+if not found:
+    print("No non repeated character found")
+    
