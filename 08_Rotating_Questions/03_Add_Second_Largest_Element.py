a494c45ecd44b004a16efc50ac71b454e050f310
AbhinavSachan <abhinavsachan02022006@gmail.com>
2026-06-26 22:37:33 +0530

    Add Second Largest Element problem solution

---
+++ 08_Rotating_Questions/03_Add_Second_Largest_Element.py
@@ -0,0 +1,21 @@
+x = int(input("Enter the size of list: "))
+
+n = []
+for i in range(x):
+    m = int(input("Enter the number: "))
+    n.append(m)
+
+largest = float("-inf")
+second_largest = float("-inf")
+
+for num in n:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num>second_largest and num!=largest :
+        second_largest = num
+
+if second_largest == float('-inf'):
+    print("No second largest element")
+else:
+    print("Second largest element:", second_largest)
